16ce3f3237b9b7e76027c6d298b9e9d044fc586e
Gokul-sami <gokulpandiyan6@gmail.com>
2024-12-24 18:39:09 +0530

    new changes

---
+++ script.js
@@ -0,0 +1,15 @@
+function menuHighlight1(){
+    document.getElementById("home").style.color = 'white';
+}
+
+function menuHighlight2(){
+    document.getElementById("projects").style.color = 'white';
+}
+
+function changeImg(){
+    document.getElementById("pro_pic").src = "images/cap_pic_edit.jpg";
+}
+
+function changeImgBack(){
+    document.getElementById("pro_pic").src = "images/cap_pic_edit_1.jpg";
+}
